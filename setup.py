5ee42da8e16900da42551440f382529f05928b39
Erik Carl Candela Rojas <erik.candela.rojas@gmail.com>
2023-09-05 19:30:08 -0500

    cambio de versio

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 from sys import version_info
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), '/')
 
 
